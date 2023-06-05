4f9af01da29dd415f41c24dfb9e85e97f36bf2f6
Sascha Lammers <sascha_lammers@gmx.de>
2023-06-05 11:01:17 -0700

    updated KFCGfx and extra_script.py

--- KFCGfx/extra_script.py
+++ KFCGfx/extra_script.py
@@ -1,5 +1,8 @@
 Import('env')
 
+# NOTE: this script is not working unless KFCGfx is separated from KFCLibrary
+# all *.cpp files have an `#if HAVE_KFCGFXLIB` statement
+
 build_src_filter = ["-<*>"]
 for item in env.get("CPPDEFINES", []):
     if isinstance(item, tuple) and item[0] == "HAVE_KFCGFXLIB" and item[1] != 0:
